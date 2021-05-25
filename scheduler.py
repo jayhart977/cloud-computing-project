a6e89fcf1ef8265c1cf88c6b0bad5f5855c88b65
jieliao <ljie9773@gmail.com>
2021-05-25 15:12:25 +0800

    2

--- scheduler.py
+++ scheduler.py
@@ -127,8 +127,8 @@ class memcached(object):
                 parsec_available_cpu = 1
                 self.memca_used_cpu -= 1
                 self.refresh()
-'''
-        elif ((self.memca_up_counter > self.memca_counter_thrd) and (self.memca_cpu_utilization_new >= self.memca_change_bound)):
+
+        '''elif ((self.memca_up_counter > self.memca_counter_thrd) and (self.memca_cpu_utilization_new >= self.memca_change_bound)):
             # Up Counter reaches thre
             if (self.memca_used_cpu == 1):
                 parsec_stat.C1_container.reload()
@@ -146,11 +146,11 @@ class memcached(object):
                 memcached_resource_set("0", self.pid)
                 parsec_available_cpu = 1
                 self.memca_used_cpu -= 1
-                self.refresh()
-'''            
-        def refresh(self):
-            self.memca_up_counter = 0
-            self.memca_down_counter = 0
+                self.refresh() '''
+            
+    def refresh(self):
+        self.memca_up_counter = 0
+        self.memca_down_counter = 0
 
 ### PARSEC
 class parsec(object):
